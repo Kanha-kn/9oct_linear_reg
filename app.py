4a6a9e555ac9e566174c7db77e6db606708f7fb0
Kanha Nahak <kanhanahak17@gmail.com>
2024-10-23 13:30:22 +0530

    v

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ st.image("_img.jpeg")
 house_size=st.number_input("please enter size of the house")
 bedrooms=st.number_input("please enter no of bedrooms")
 
-l_model=joblib.load('predict.pkl')
+model=joblib.load('predict.pkl')
 
 if st.button("predict"):
     features = np.array([[house_size,bedrooms]])
-    output= l_model.predict(features)
+    output=model.predict(features)
     st.write(f"the price of the house is {output[0]}")
